f640bc1f19a3a11f1ec2d6296c5d3b0c0ca5a6a8
MHSanaei <mc.sanaei@gmail.com>
2023-02-21 11:50:09 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -62,7 +62,7 @@ if [[ x"${release}" == x"centos" ]]; then
         echo -e "${red} Please use CentOS 8 or higher ${plain}\n" && exit 1
     fi
 elif [[ x"${release}" == x"ubuntu" ]]; then
-    if [[ ${os_version} -lt 18 ]]; then
+    if [[ ${os_version} -lt 20 ]]; then
         echo -e "${red} Please use Ubuntu 20 or higher ${plain}\n" && exit 1
     fi
 elif [[ x"${release}" == x"debian" ]]; then
