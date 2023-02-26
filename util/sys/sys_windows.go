6a38b8aed76fbf7812393ea628ca312130ae7e9b
MHSanaei <33454419+MHSanaei@users.noreply.github.com>
2023-02-26 17:29:54 +0330

    tcp-udp count windows (debug)

---
+++ util/sys/sys_windows.go
@@ -0,0 +1,24 @@
+//go:build windows
+// +build windows
+
+package sys
+
+import (
+	"github.com/shirou/gopsutil/v3/net"
+)
+
+func GetTCPCount() (int, error) {
+	stats, err := net.Connections("tcp")
+	if err != nil {
+		return 0, err
+	}
+	return len(stats), nil
+}
+
+func GetUDPCount() (int, error) {
+	stats, err := net.Connections("udp")
+	if err != nil {
+		return 0, err
+	}
+	return len(stats), nil
+}
