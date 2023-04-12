b4727cc5f8aa8507f111454038d3b76087980610
MHSanaei <ho3ein.sanaei@gmail.com>
2023-04-12 23:41:34 +0330

    update speedtest link
    https://www.speedtest.net/apps/cli

--- x-ui.sh
+++ x-ui.sh
@@ -726,19 +726,19 @@ run_speedtest() {
         # If not installed, install it
         if command -v dnf &> /dev/null; then
             sudo dnf install -y curl
-            curl -s https://install.speedtest.net/app/cli/install.rpm.sh | sudo bash
+            curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.rpm.sh | sudo bash
             sudo dnf install -y speedtest
         elif command -v yum &> /dev/null; then
             sudo yum install -y curl
-            curl -s https://install.speedtest.net/app/cli/install.rpm.sh | sudo bash
+            curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.rpm.sh | sudo bash
             sudo yum install -y speedtest
         elif command -v apt-get &> /dev/null; then
             sudo apt-get update && sudo apt-get install -y curl
-            curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
+            curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
             sudo apt-get install -y speedtest
         elif command -v apt &> /dev/null; then
             sudo apt update && sudo apt install -y curl
-            curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
+            curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
             sudo apt install -y speedtest
         else
             echo "Error: Package manager not found. You may need to install Speedtest manually."
