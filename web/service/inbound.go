c6b59f1ee3ad4470a15aac2a6f2b0f62be260788
MHSanaei <ho3ein.sanaei@gmail.com>
2023-04-13 18:45:18 +0330

    [migration] Remove Orphaned Traffics
    Co-Authored-By: Alireza Ahmadi <alireza7@gmail.com>

--- web/service/inbound.go
+++ web/service/inbound.go
@@ -507,6 +507,17 @@ func (s *InboundService) DisableInvalidInbounds() (int64, error) {
 	count := result.RowsAffected
 	return count, err
 }
+func (s *InboundService) RemoveOrphanedTraffics() {
+	db := database.GetDB()
+	db.Exec(`
+		DELETE FROM client_traffics
+		WHERE email NOT IN (
+			SELECT JSON_EXTRACT(client.value, '$.email')
+			FROM inbounds,
+				JSON_EACH(JSON_EXTRACT(inbounds.settings, '$.clients')) AS client
+		)
+	`)
+}
 func (s *InboundService) DisableInvalidClients() (int64, error) {
 	db := database.GetDB()
 	now := time.Now().Unix() * 1000
