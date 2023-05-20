9b7cddbec747af9d3a70dddb1179ecb14659d67e
Hamidreza Ghavami <70919649+hamid-gh98@users.noreply.github.com>
2023-05-20 19:51:45 +0430

    update to use WebI18n func

--- web/controller/util.go
+++ web/controller/util.go
@@ -38,12 +38,12 @@ func jsonMsgObj(c *gin.Context, msg string, obj interface{}, err error) {
 	if err == nil {
 		m.Success = true
 		if msg != "" {
-			m.Msg = msg + I18n(c, "success")
+			m.Msg = msg + WebI18n(c, "success")
 		}
 	} else {
 		m.Success = false
-		m.Msg = msg + I18n(c, "fail") + ": " + err.Error()
-		logger.Warning(msg+I18n(c, "fail")+": ", err)
+		m.Msg = msg + WebI18n(c, "fail") + ": " + err.Error()
+		logger.Warning(msg+WebI18n(c, "fail")+": ", err)
 	}
 	c.JSON(http.StatusOK, m)
 }
