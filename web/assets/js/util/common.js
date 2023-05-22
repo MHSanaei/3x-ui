ffa23a43c62b594fc7751cb85096d05085910b6b
MHSanaei <ho3ein.sanaei@gmail.com>
2023-05-22 16:40:08 +0330

    update - URIComponent just for cookie value

--- web/assets/js/util/common.js
+++ web/assets/js/util/common.js
@@ -70,25 +70,27 @@ function debounce(fn, delay) {
 
 function getCookie(cname) {
     let name = cname + '=';
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
+    let ca = document.cookie.split(';');
     for (let i = 0; i < ca.length; i++) {
         let c = ca[i];
         while (c.charAt(0) == ' ') {
             c = c.substring(1);
         }
         if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
+            // decode cookie value only
+            return decodeURIComponent(c.substring(name.length, c.length));
         }
     }
     return '';
 }
 
+
 function setCookie(cname, cvalue, exdays) {
     const d = new Date();
     d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
     let expires = 'expires=' + d.toUTCString();
-    document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
+    // encode cookie value
+    document.cookie = cname + '=' + encodeURIComponent(cvalue) + ';' + expires + ';path=/';
 }
 
 function usageColor(data, threshold, total) {
