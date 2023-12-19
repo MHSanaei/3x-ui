a2bdf23940ffafabbee5329472aea8473af1aec5
quydang <81231812+quydang04@users.noreply.github.com>
2023-12-19 17:27:04 +0700

    Update support for new architecture ARM devices and added support for more OS. (#1389)
    * Add files via upload
    
    * Delete web/translation/translate.vi_VN.toml
    
    * Add files via upload
    
    * Update translate.vi_VN.toml
    
    * Update release.yml
    
    * Update install.sh
    
    * Update install.sh
    
    * Update install.sh
    
    * Update install.sh
    
    * Update x-ui.sh
    
    * Update docker.yml
    
    * Update DockerInit.sh
    
    * Update install.sh
    
    * Update install.sh
    
    * Update install.sh
    
    * Update .github/workflows/release.yml
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Ho3ein <ho3ein.sanaei@gmail.com>

--- DockerInit.sh
+++ DockerInit.sh
@@ -9,6 +9,10 @@ case $1 in
         ARCH="arm64-v8a"
         FNAME="arm64"
         ;;
+    armv7 | arm | arm32)
+        ARCH="arm32-v7a"
+        FNAME="arm32"
+        ;;
     *)
         ARCH="64"
         FNAME="amd64"
@@ -20,10 +24,12 @@ cd build/bin
 
 wget "https://github.com/XTLS/Xray-core/releases/download/v1.8.6/Xray-linux-${ARCH}.zip"
 unzip "Xray-linux-${ARCH}.zip"
-rm -f "Xray-linux-${ARCH}.zip" geoip.dat geosite.dat geoip_IR.dat geosite_IR.dat 
+rm -f "Xray-linux-${ARCH}.zip" geoip.dat geosite.dat geoip_IR.dat geosite_IR.dat geoip_VN.dat geosite_VN.dat
 mv xray "xray-linux-${FNAME}"
 
 wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
 wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
 wget -O geoip_IR.dat https://github.com/chocolate4u/Iran-v2ray-rules/releases/latest/download/geoip.dat
 wget -O geosite_IR.dat https://github.com/chocolate4u/Iran-v2ray-rules/releases/latest/download/geosite.dat
+wget -O geoip_VN.dat https://github.com/vuong2023/vn-v2ray-rules/releases/latest/download/geoip.dat
+wget -O geosite_VN.dat https://github.com/vuong2023/vn-v2ray-rules/releases/latest/download/geosite.dat
