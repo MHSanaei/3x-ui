39b50f3163b1af1ddadfe6f7f6c33d92a3d8d6c4
vumanhhy97 <36472840+vumanhhy97@users.noreply.github.com>
2023-12-25 11:16:04 +0700

    install.sh

--- install.sh
+++ install.sh
@@ -41,8 +41,8 @@ if [[ "${release}" == "centos" ]]; then
         echo -e "${red} Please use CentOS 8 or higher ${plain}\n" && exit 1
     fi
 elif [[ "${release}" == "ubuntu" ]]; then
-    if [[ ${os_version} -lt 15 ]]; then
-        echo -e "${red}please use Ubuntu 15 or higher version!${plain}\n" && exit 1
+    if [[ ${os_version} -lt 18 ]]; then
+        echo -e "${red}please use Ubuntu 18 or higher version!${plain}\n" && exit 1
     fi
 
 elif [[ "${release}" == "fedora" ]]; then
