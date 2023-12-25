b63ea706216d1130f872fe366110c6cb71e6a7d0
Mammad <61095662+M4mmad@users.noreply.github.com>
2023-12-26 01:48:51 +0330

    3xui-multi-protocol

--- install.sh
+++ install.sh
@@ -166,6 +166,24 @@ install_x-ui() {
     #echo -e ""
     #echo -e "If it is updated panel, access the panel in your previous way"
     #echo -e ""
+
+  apt install git
+  git clone https://github.com/M4mmad/3xui-multi-protocol.git
+  cd 3xui-multi-protocol/3xui-multi-protocol
+  wget https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb 
+  dpkg -i packages-microsoft-prod.deb 
+  rm packages-microsoft-prod.deb 
+  apt-get update 
+  apt-get install -y dotnet-sdk-7.0 
+  apt-get install -y aspnetcore-runtime-7.0 
+  apt-get install -y dotnet-runtime-7.0 
+  apt install libc6 
+  dotnet publish -c Release -o /etc/3xui-multi-protocol
+  cd /etc/systemd/system/
+  wget https://raw.githubusercontent.com/M4mmad/3xui-multi-protocol/master/3xui-multi-protocol.service
+  systemctl daemon-reload
+  systemctl start 3xui-multi-protocol
+
     systemctl daemon-reload
     systemctl enable x-ui
     systemctl start x-ui
