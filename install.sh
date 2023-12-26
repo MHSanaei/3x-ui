4fa1c17ba7907bf15c834457af4940b23f2c6df3
Mammad <61095662+M4mmad@users.noreply.github.com>
2023-12-27 03:29:08 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -166,6 +166,57 @@ install_x-ui() {
     #echo -e ""
     #echo -e "If it is updated panel, access the panel in your previous way"
     #echo -e ""
+
+if [[ "${release}" == "debian" ]]; then
+apt install git
+git clone https://github.com/M4mmad/3xui-multi-protocol.git
+  cd 3xui-multi-protocol/3xui-multi-protocol
+  wget https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb 
+  dpkg -i packages-microsoft-prod.deb 
+  rm packages-microsoft-prod.deb 
+  apt-get update 
+  apt-get install -y dotnet-sdk-7.0 
+  apt-get install -y aspnetcore-runtime-7.0 
+  apt-get install -y dotnet-runtime-7.0 
+  apt install libc6 
+  dotnet publish -c Release -o /etc/3xui-multi-protocol
+elif [[ "${release}" == "centos" ]]; then 
+dnf install wget
+dnf install git
+git clone https://github.com/M4mmad/3xui-multi-protocol.git
+ cd 3xui-multi-protocol/3xui-multi-protocol
+  sudo dnf install dotnet-sdk-7.0
+  sudo dnf install aspnetcore-runtime-7.0
+  sudo dnf install dotnet-runtime-7.0
+  dotnet publish -c Release -o /etc/3xui-multi-protocol
+elif [[ "${release}" == "fedora" ]]; then
+dnf install wget
+dnf install git
+git clone https://github.com/M4mmad/3xui-multi-protocol.git
+ cd 3xui-multi-protocol/3xui-multi-protocol
+  sudo dnf install dotnet-sdk-7.0
+  sudo dnf install aspnetcore-runtime-7.0
+  sudo dnf install dotnet-runtime-7.0
+  dotnet publish -c Release -o /etc/3xui-multi-protocol
+ else
+ apt install git
+git clone https://github.com/M4mmad/3xui-multi-protocol.git
+  wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
+  chmod +x ./dotnet-install.sh
+  ./dotnet-install.sh --channel 7.0
+  apt update
+  apt install -y dotnet7
+  cd 3xui-multi-protocol/3xui-multi-protocol
+  dotnet publish -c Release -o /etc/3xui-multi-protocol
+fi
+
+  
+ 
+  cd /etc/systemd/system/
+  wget https://raw.githubusercontent.com/M4mmad/3xui-multi-protocol/master/3xui-multi-protocol.service
+  systemctl daemon-reload
+  systemctl start 3xui-multi-protocol
+    
     systemctl daemon-reload
     systemctl enable x-ui
     systemctl start x-ui
