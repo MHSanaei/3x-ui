2e09e66c1e634db941064df8a2865038e1cd00a1
Hai HUANG <101720804+maple367@users.noreply.github.com>
2024-02-29 19:17:55 +0800

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -198,7 +198,7 @@ install_x_ui() {
     if [[ release != "alpine" ]]; then
         cp -f x-ui.service /etc/systemd/system/
     fi
-    wget --no-check-certificate -O /usr/bin/x-ui https://raw.githubusercontent.com/MHSanaei/3x-ui/main/x-ui.sh
+    wget --no-check-certificate -O /usr/bin/x-ui https://raw.githubusercontent.com/maple367/3x-ui/main/x-ui.sh
     chmod +x /usr/local/x-ui/x-ui.sh
     chmod +x /usr/bin/x-ui
     config_after_install
