904a90bb1e0f6d2223b2a56e812e910b9f56ab88
Hai HUANG <101720804+maple367@users.noreply.github.com>
2024-02-29 19:09:57 +0800

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -139,8 +139,11 @@ config_after_install() {
             makedir /lib64
         fi
         ln /lib/ld-musl-x86_64.so.1 /lib64/ld-linux-x86-64.so.2
-        apk add gcompat glibc libc6-compat
-        ln -s /lib/libc.so.6 /usr/lib/libresolv.so.2
+        wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub
+        wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.35-r1/glibc-2.35-r1.apk
+        apk add glibc-2.35-r1.apk
+        rm -f glibc-2.35-r1.apk
+        # ln -s /lib/libc.so.6 /usr/lib/libresolv.so.2
     fi
     /usr/local/x-ui/x-ui migrate
 }
