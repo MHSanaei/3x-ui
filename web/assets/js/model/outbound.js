2f0315785f45ab5ed70667c25d8c4cc083f71f5b
TaraRostami <132676256+TaraRostami@users.noreply.github.com>
2024-04-17 23:05:00 +0330

    UI Improvements
    Better Table
    Update QR Code Modal
    Better Info Modal
    Compression HTML files
    Better Dropdown Menu
    Better Calendar
    and more ..
    
    Remove files
    
    Minor Fixes

--- web/assets/js/model/outbound.js
+++ web/assets/js/model/outbound.js
@@ -690,12 +690,12 @@ class Outbound extends CommonClass {
                 url.searchParams.get('quicSecurity') ?? 'none',
                 url.searchParams.get('key') ?? '',
                 headerType ?? 'none');
-            } else if (type === 'grpc') {
-                stream.grpc = new GrpcStreamSettings(
-                    url.searchParams.get('serviceName') ?? '',
-                    url.searchParams.get('authority') ?? '',
-                    url.searchParams.get('mode') == 'multi');
-            } else if (type === 'httpupgrade') {
+        } else if (type === 'grpc') {
+            stream.grpc = new GrpcStreamSettings(
+                url.searchParams.get('serviceName') ?? '',
+                url.searchParams.get('authority') ?? '',
+                url.searchParams.get('mode') == 'multi');
+        } else if (type === 'httpupgrade') {
             stream.httpupgrade = new HttpUpgradeStreamSettings(path,host);
         }
 
