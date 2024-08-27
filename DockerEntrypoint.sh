59fcc4002ce4d8819a45eaf25dc6ea1a7f91a2e1
OnceUponATimeInAmerica <114700833+OnceUponATimeInAmerica@users.noreply.github.com>
2024-08-27 04:26:55 +0330

    Update DockerEntrypoint.sh (test2)

--- DockerEntrypoint.sh
+++ DockerEntrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+# Run sshd
+/usr/sbin/sshd
+
 # Start nginx in the background
 nginx
 
 # Start fail2ban
 ## fail2ban-client -x start
 
-# Run x-ui
+# Run x-ui as main process
 exec /app/x-ui
