860fae8a4faf2e74048752cd2645cf8540f2cca6
OnceUponATimeInAmerica <114700833+OnceUponATimeInAmerica@users.noreply.github.com>
2024-08-27 08:59:20 +0330

    Update DockerEntrypoint.sh (test3)

--- DockerEntrypoint.sh
+++ DockerEntrypoint.sh
@@ -3,8 +3,8 @@
 # Check if the file exists
 if [ ! -f "/root/x-ui_copied.db" ]; then
   echo "DB file not found. Downloading..."
-  curl -L -o /root/ui_copied.db "https://drive.google.com/uc?export=download&id=${FILE_ID}"
-  curl -L -o /x-ui/x-ui.db "https://drive.google.com/uc?export=download&id=${FILE_ID}"
+  curl -L -o /root/x-ui_copied.db "https://drive.google.com/uc?export=download&id=${FILE_ID}"
+  curl -L -o /etc/x-ui/x-ui.db "https://drive.google.com/uc?export=download&id=${FILE_ID}"
 else
   echo "DB file already exists. Skipping download."
 fi
