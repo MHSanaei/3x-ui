663cf5649f7a43c3f37bef6173333a3524133b54
mhsanaei <ho3ein.sanaei@gmail.com>
2024-09-12 09:41:24 +0200

    Session - default 60 minute (minimum)

--- web/assets/js/model/setting.js
+++ web/assets/js/model/setting.js
@@ -7,7 +7,7 @@ class AllSetting {
         this.webCertFile = "";
         this.webKeyFile = "";
         this.webBasePath = "/";
-        this.sessionMaxAge = 0;
+        this.sessionMaxAge = 60;
         this.pageSize = 50;
         this.expireDiff = 0;
         this.trafficDiff = 0;
