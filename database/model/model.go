d18a1a37ceb7c005a5a829ba8b9ef2d103c181b5
Sanaei <ho3ein.sanaei@gmail.com>
2025-02-04 11:27:58 +0100

    revert group management (#2656)
    * Revert "json post base path bug fixed (#2647)"
    
    This reverts commit 04cf250a547bb64265d256e7d15af7cea5ecfa67.
    
    * Revert "Group Management of Subscription Clients"
    
    * Revert "fix getSubGroupClients for enable/disable and edit clients."
    
    * Revert "Enhance database initialization in db.go (#2645)"
    
    This reverts commit 66fe84181b9c4e2f6c6be943a7f486b4308c32ff.
    
    * Revert "Add checkpoint handling in CloseDB function (#2646)"
    
    This reverts commit 4dd40f6f192e3f94f2ea4fe9e942e6663b5a1527.
    
    * Revert "Improved database model migration and added indexing (#2655)"
    
    This reverts commit b922d986d6783ce28d00ca948024dee44a11f29e.

--- database/model/model.go
+++ database/model/model.go
@@ -29,14 +29,14 @@ type User struct {
 
 type Inbound struct {
 	Id          int                  `json:"id" form:"id" gorm:"primaryKey;autoIncrement"`
-	UserId      int                  `json:"-" gorm:"index"`
+	UserId      int                  `json:"-"`
 	Up          int64                `json:"up" form:"up"`
 	Down        int64                `json:"down" form:"down"`
 	Total       int64                `json:"total" form:"total"`
 	Remark      string               `json:"remark" form:"remark"`
 	Enable      bool                 `json:"enable" form:"enable"`
 	ExpiryTime  int64                `json:"expiryTime" form:"expiryTime"`
-	ClientStats []xray.ClientTraffic `gorm:"foreignKey:InboundId;references:Id;constraint:OnDelete:CASCADE" json:"clientStats"`
+	ClientStats []xray.ClientTraffic `gorm:"foreignKey:InboundId;references:Id" json:"clientStats" form:"clientStats"`
 
 	// config part
 	Listen         string   `json:"listen" form:"listen"`
--- xray/client_traffic.go
+++ xray/client_traffic.go
@@ -2,9 +2,9 @@ package xray
 
 type ClientTraffic struct {
 	Id         int    `json:"id" form:"id" gorm:"primaryKey;autoIncrement"`
-	InboundId  int    `json:"inboundId" form:"inboundId" gorm:"index;not null"`
+	InboundId  int    `json:"inboundId" form:"inboundId"`
 	Enable     bool   `json:"enable" form:"enable"`
-	Email      string `json:"email" form:"email" gorm:"uniqueIndex"`
+	Email      string `json:"email" form:"email" gorm:"unique"`
 	Up         int64  `json:"up" form:"up"`
 	Down       int64  `json:"down" form:"down"`
 	ExpiryTime int64  `json:"expiryTime" form:"expiryTime"`
