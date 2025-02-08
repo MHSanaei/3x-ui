6eb436b2d28f7c905a8a1ff87a9993bfdd04dab6
root <root@vm3688062.stark-industries.solutions>
2025-02-08 23:59:03 +0100

    Add ExternalTrafficURL Post request call

--- web/assets/js/model/setting.js
+++ web/assets/js/model/setting.js
@@ -31,7 +31,7 @@ class AllSetting {
         this.subPath = "/sub/";
         this.subJsonPath = "/json/";
         this.subDomain = "";
-        this.externalInformEnable = false;
+        this.externalTrafficInformEnable = false;
         this.externalTrafficInformURI = "";
         this.subCertFile = "";
         this.subKeyFile = "";
