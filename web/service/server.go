c27b7c8ee356b22ad75aa9feb32c9912c130d141
mhsanaei <ho3ein.sanaei@gmail.com>
2025-03-01 10:19:52 +0100

    fix mistake

--- web/service/server.go
+++ web/service/server.go
@@ -285,7 +285,7 @@ func (s *ServerService) GetXrayVersions() ([]string, error) {
 			continue
 		}
 
-		if (major >= 25 && minor >= 2 && patch >= 18) || (major >= 26) {
+		if major > 25 || (major == 25 && minor > 2) || (major == 25 && minor == 2 && patch >= 18) {
 			versions = append(versions, release.TagName)
 		}
 	}
