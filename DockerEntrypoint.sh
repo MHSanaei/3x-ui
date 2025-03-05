96ee92cb842cd601ac72729cfb48f79fc2b86789
serogaq <36307024+serogaq@users.noreply.github.com>
2025-03-05 20:38:39 +0300

    delete docker logs

--- DockerEntrypoint.sh
+++ DockerEntrypoint.sh
@@ -3,9 +3,5 @@
 # Start fail2ban
 [ $X_UI_ENABLE_FAIL2BAN == "true" ] && fail2ban-client -x start
 
-# Docker Logs
-#ln -sf /dev/stdout /app/access.log
-#ln -sf /dev/stdout /app/error.log
-
 # Run x-ui
 exec /app/x-ui
