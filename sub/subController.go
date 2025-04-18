dc323c8d9081587bf5ecc20a8006f3ca618a5626
io-vpn <justchilllguy@outlook.com>
2025-04-19 01:15:16 +0300

    Update subController.go

--- sub/subController.go
+++ sub/subController.go
@@ -85,7 +85,7 @@ func (a *SUBController) subs(c *gin.Context) {
 		// Add headers
 		c.Writer.Header().Set("Subscription-Userinfo", header)
 		c.Writer.Header().Set("Profile-Update-Interval", a.updateInterval)
-		c.Writer.Header().Set("Profile-Title", "base64:" + base64.StdEncoding.EncodeToString([]byte(a.subTitle)))
+		c.Writer.Header().Set("Profile-Title", "iovpn")
 
 		if a.subEncrypt {
 			c.String(200, base64.StdEncoding.EncodeToString([]byte(result)))
