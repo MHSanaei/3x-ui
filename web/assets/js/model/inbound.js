01f984e054fe3c6e06aeb9b947ef009fbfe5db65
Alireza Ahmadi <alireza7@gmail.com>
2025-08-04 16:45:09 +0200

    add dokodemo port mapping

--- web/assets/js/model/inbound.js
+++ web/assets/js/model/inbound.js
@@ -2312,12 +2312,14 @@ Inbound.DokodemoSettings = class extends Inbound.Settings {
         protocol,
         address,
         port,
+        portMap = [],
         network = 'tcp,udp',
         followRedirect = false
     ) {
         super(protocol);
         this.address = address;
         this.port = port;
+        this.portMap = portMap;
         this.network = network;
         this.followRedirect = followRedirect;
     }
@@ -2327,6 +2329,7 @@ Inbound.DokodemoSettings = class extends Inbound.Settings {
             Protocols.DOKODEMO,
             json.address,
             json.port,
+            XrayCommonClass.toHeaders(json.portMap),
             json.network,
             json.followRedirect,
         );
@@ -2336,6 +2339,7 @@ Inbound.DokodemoSettings = class extends Inbound.Settings {
         return {
             address: this.address,
             port: this.port,
+            portMap: XrayCommonClass.toV2Headers(this.portMap),
             network: this.network,
             followRedirect: this.followRedirect,
         };
