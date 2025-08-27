42ce9a31d4a6cfc5a34c6c7112cba0f41b1996a7
Ali Golzar <agt.ali.gr@gmail.com>
2025-08-27 05:55:47 +0330

    feat(db): add allTime field to Inbound and ClientTraffic models

--- xray/client_traffic.go
+++ xray/client_traffic.go
@@ -7,6 +7,7 @@ type ClientTraffic struct {
 	Email      string `json:"email" form:"email" gorm:"unique"`
 	Up         int64  `json:"up" form:"up"`
 	Down       int64  `json:"down" form:"down"`
+	AllTime    int64  `json:"allTime" form:"allTime"`
 	ExpiryTime int64  `json:"expiryTime" form:"expiryTime"`
 	Total      int64  `json:"total" form:"total"`
 	Reset      int    `json:"reset" form:"reset" gorm:"default:0"`
