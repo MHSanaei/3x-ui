fcdeb1fc79574175df0021bf4cdf03438eab0620
RahGozar <info@developermen.ir>
2025-09-17 19:15:28 +0330

    feat: add UUID to ClientTraffic (#3491)
    * Update client_traffic.go
    
    * Update inbound.go

--- web/service/inbound.go
+++ web/service/inbound.go
@@ -1953,6 +1953,7 @@ func (s *InboundService) GetClientTrafficByEmail(email string) (traffic *xray.Cl
 	if t != nil && client != nil {
 		t.Enable = client.Enable
 		t.SubId = client.SubID
+		t.UUID = client.ID
 		return t, nil
 	}
 	return nil, nil
@@ -1994,6 +1995,7 @@ func (s *InboundService) GetClientTrafficByID(id string) ([]xray.ClientTraffic, 
 		if ct, client, e := s.GetClientByEmail(traffics[i].Email); e == nil && ct != nil && client != nil {
 			traffics[i].Enable = client.Enable
 			traffics[i].SubId = client.SubID
+			traffics[i].UUID = client.ID
 		}
 	}
 	return traffics, err
