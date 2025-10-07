0e28f2b1789a7fbf8855dad4e757af9c254cd618
Dikiy13371 <css81933@gmail.com>
2025-10-07 23:27:30 +0300

    delete type user struct in model.go

--- database/model/model.go
+++ database/model/model.go
@@ -23,13 +23,6 @@ const (
 	WireGuard   Protocol = "wireguard"
 )
 
-// User represents a user account in the 3x-ui panel.
-type User struct {
-	Id       int    `json:"id" gorm:"primaryKey;autoIncrement"`
-	Username string `json:"username"`
-	Password string `json:"password"`
-}
-
 // Inbound represents an Xray inbound configuration with traffic statistics and settings.
 type Inbound struct {
 	Id                   int                  `json:"id" form:"id" gorm:"primaryKey;autoIncrement"`                                                    // Unique identifier
