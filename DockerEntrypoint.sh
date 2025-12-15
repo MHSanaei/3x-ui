5bd52eff0ccf3833db814418df3be75a981e1f70
Michael S2pac <mixa21.11@mail.ru>
2025-12-15 15:30:20 +0300

    Refactored entrypoint for 3xui container

--- DockerEntrypoint.sh
+++ DockerEntrypoint.sh
@@ -2,18 +2,17 @@
 
 FINISH_FILE="$GEODATA_DIR/cron-job-finished.txt"
 
-MAX_WAIT=300  # 5 minutes
 ELAPSED=0
 INTERVAL=10
 
-while [ ! -f "$FINISH_FILE" ] && [ $ELAPSED -lt $MAX_WAIT ]; do
-  echo "Still waiting for geodata initialization... ($ELAPSED/$MAX_WAIT seconds)"
+while [ ! -f "$FINISH_FILE" ] && [ "$ELAPSED" -lt "$MAX_GEODATA_DIR_WAIT" ]; do
+  echo "Waiting for geodata initialization... ($ELAPSED/$MAX_GEODATA_DIR_WAIT seconds)"
   sleep $INTERVAL
   ELAPSED=$((ELAPSED + INTERVAL))
 done
 
 if [ ! -f "$FINISH_FILE" ]; then
-  echo "ERROR: Geodata initialization timed out after $MAX_WAIT seconds"
+  echo "ERROR: Geodata initialization timed out after $MAX_GEODATA_DIR_WAIT seconds"
   echo "Container startup aborted."
   exit 1
 fi
