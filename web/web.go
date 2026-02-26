dad03fc2a74c079226fc8be311bba8629dd18595
Nabi <nabikaz60@gmail.com>
2026-02-26 08:26:47 +0330

    fix: remove excluded paths from gzip middleware in router initialization

--- web/web.go
+++ web/web.go
@@ -200,7 +200,7 @@ func (s *Server) initRouter() (*gin.Engine, error) {
 	if err != nil {
 		return nil, err
 	}
-	engine.Use(gzip.Gzip(gzip.DefaultCompression, gzip.WithExcludedPaths([]string{basePath + "panel/api/"})))
+	engine.Use(gzip.Gzip(gzip.DefaultCompression))
 	assetsBasePath := basePath + "assets/"
 
 	store := cookie.NewStore(secret)
