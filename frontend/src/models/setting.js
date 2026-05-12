93784b8afcdcc391609d0b1cc697b79bcf81c5a5
Vladislav Kobzev <kp42@ya.ru>
2026-05-12 10:43:52 +0300

    Add possibility to remove client email from sub

--- frontend/src/models/setting.js
+++ frontend/src/models/setting.js
@@ -56,6 +56,7 @@ export class AllSetting {
         this.subUpdates = 12;
         this.subEncrypt = true;
         this.subShowInfo = true;
+        this.subEmailInRemark = true;
         this.subURI = "";
         this.subJsonURI = "";
         this.subClashURI = "";
