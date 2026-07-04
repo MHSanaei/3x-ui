7cc815c5f39d1960701c3b873b6a57032a9564de
KimaruBs <kiriljaxla@gmail.com>
2026-07-04 15:08:22 +0300

    Update routes.tsx

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -12,7 +12,7 @@ const HostsPage = lazy(() => import('@/pages/hosts/HostsPage'));
 const SettingsPage = lazy(() => import('@/pages/settings/SettingsPage'));
 const XrayPage = lazy(() => import('@/pages/xray/XrayPage'));
 const ApiDocsPage = lazy(() => import('@/pages/api-docs/ApiDocsPage'));
-const Tgbot = lazy(() => import ('@/pages/tgbot/TgBotPage'));
+const TgBotPage = lazy(() => import ('@/pages/tgbot/TgBotPage'));
 
 function withSuspense(node: React.ReactNode) {
   return <Suspense fallback={null}>{node}</Suspense>;
