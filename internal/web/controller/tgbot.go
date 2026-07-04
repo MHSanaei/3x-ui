da5ed78bae3a49d85dd9b2fd0e7f635e75b19840
KimaruBs <kiriljaxla@gmail.com>
2026-07-04 14:51:01 +0300

    Update tgbot.go

--- internal/web/controller/tgbot.go
+++ internal/web/controller/tgbot.go
@@ -1,1 +1,29 @@
+package controller
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type TgBotController struct {
+	BaseController
+}
+
+func NewTgBotController(g *gin.RouterGroup) *TgBotController {
+	a := &TgBotController{}
+	a.initRouter(g)
+	return a
+}
+
+func (a *TgBotController) initRouter(g *gin.RouterGroup) {
+	gg := g.Group("/tgbot")
+	gg.GET("/status", a.getStatus)
+}
+
+func (a *TgBotController) getStatus(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"success": true,
+		"obj":     "ok",
+	})
+}
