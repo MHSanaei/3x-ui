7e43f77c7bba6d6f30823ff38fe1a6c4f6494497
KimaruBs <kiriljaxla@gmail.com>
2026-07-04 15:16:46 +0300

    Update TgBotPage.tsx

--- frontend/src/pages/tgbot/TgBotPage.tsx
+++ frontend/src/pages/tgbot/TgBotPage.tsx
@@ -1,7 +1,28 @@
-export default function TelegramPage() {
+import { useMemo } from 'react';
+import { ConfigProvider, Layout } from 'antd';
+import { useTheme } from '@/hooks/useTheme';
+import AppSidebar from '@/layouts/AppSidebar';
+
+export default function TgBotPage() {
+  const { isDark, isUltra, antdThemeConfig } = useTheme();
+
+  const pageClass = useMemo(() => {
+    const classes = ['tgbot-page'];
+    if (isDark) classes.push('is-dark');
+    if (isUltra) classes.push('is-ultra');
+    return classes.join(' ');
+  }, [isDark, isUltra]);
+
   return (
-    <div>
-      <h1>Telegram Bot</h1>
-    </div>
+    <ConfigProvider theme={antdThemeConfig}>
+      <Layout className={pageClass}>
+        <AppSidebar />
+        <Layout className="content-shell">
+          <Layout.Content className="content-area">
+            <h1>Telegram Bot</h1>
+          </Layout.Content>
+        </Layout>
+      </Layout>
+    </ConfigProvider>
   );
 }
