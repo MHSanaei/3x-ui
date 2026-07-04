9e3cb0aa687a1401d099be0982282efb570391a9
KimaruBs <kiriljaxla@gmail.com>
2026-07-04 14:22:59 +0300

    Update routes.tsx

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -12,6 +12,7 @@ const HostsPage = lazy(() => import('@/pages/hosts/HostsPage'));
 const SettingsPage = lazy(() => import('@/pages/settings/SettingsPage'));
 const XrayPage = lazy(() => import('@/pages/xray/XrayPage'));
 const ApiDocsPage = lazy(() => import('@/pages/api-docs/ApiDocsPage'));
+const Tgbot = lazy(() => import ('@/pages/tgbot/tgbot'));
 
 function withSuspense(node: React.ReactNode) {
   return <Suspense fallback={null}>{node}</Suspense>;
@@ -33,6 +34,7 @@ const routes: RouteObject[] = [
       { path: 'outbound', element: withSuspense(<XrayPage />) },
       { path: 'routing', element: withSuspense(<XrayPage />) },
       { path: 'api-docs', element: withSuspense(<ApiDocsPage />) },
+      { path: 'tgbot
     ],
   },
 ];
