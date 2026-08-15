b99a46a660044c838aa38ba4aa920b19fe3f5dd2
Sanaei <ho3ein.sanaei@gmail.com>
2026-08-15 15:21:42 +0200

    Update sub_fetch_test.go

--- internal/sub/sub_fetch_test.go
+++ internal/sub/sub_fetch_test.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+
 	"github.com/mhsanaei/3x-ui/v3/internal/database"
 	"github.com/mhsanaei/3x-ui/v3/internal/database/model"
 	"github.com/mhsanaei/3x-ui/v3/internal/xray"
