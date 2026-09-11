4a28a8a934ffe12f72badee9d225f0a65e44c19c
mrchatam <287639636+mrchatam@users.noreply.github.com>
2026-09-11 17:05:14 +0000

    style(frontend): oxfmt realityScanCandidates in setting.ts

--- frontend/src/models/setting.ts
+++ frontend/src/models/setting.ts
@@ -9,7 +9,8 @@ export class AllSetting {
   webBasePath = '/';
   sessionMaxAge = 360;
   trustedProxyCIDRs = '127.0.0.1/32,::1/128';
-  realityScanCandidates = 'www.cloudflare.com:443,www.microsoft.com:443,www.amazon.com:443,aws.amazon.com:443,www.samsung.com:443,www.nvidia.com:443,www.amd.com:443,www.intel.com:443,www.sony.com:443,dl.google.com:443';
+  realityScanCandidates =
+    'www.cloudflare.com:443,www.microsoft.com:443,www.amazon.com:443,aws.amazon.com:443,www.samsung.com:443,www.nvidia.com:443,www.amd.com:443,www.intel.com:443,www.sony.com:443,dl.google.com:443';
   ipLimitAllowlist = '';
   panelOutbound = '';
   pageSize = 25;
